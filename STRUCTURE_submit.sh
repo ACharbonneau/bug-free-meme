75d5ec08141cc4794670c18fc2ac7fc789e2977d
Amanda Charbonneau <charbo24@msu.edu>
2017-02-06 15:46:42 -0500

    submission script working

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -2,8 +2,10 @@
 
 #Launches 20 different randomizations of the dataset (seq 1 20) with a K of ( -t <number range>)		
 
-# Files were randomized by:
-#   for i in `seq 1 20`; do shuf -o R${i}_batch_20170205.structure.tsv batch_20170205.structure.tsv; done
+#Randomize Input files
+for i in `seq 1 20`; do head -2 batch_20170205.structure.tsv | tail -1 > R${i}_batch_20170205.structure.tsv; done
+for i in `seq 1 20`; do tail -n +3 batch_20170205.structure.tsv | shuf >> R${i}_batch_20170205.structure.tsv; done
+
 
 for i in `seq 1 22`
     do less Random_STRUCTURE.qsub | sed s/RUNNUMBER/R${i}_batch_20170205.structure.tsv/ > withtemp.qsub
