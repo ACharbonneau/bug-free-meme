1a0a9c25ad724ca18639682065525c22b92651a7
Amanda Charbonneau <charbo24@msu.edu>
2017-02-06 12:26:28 -0500

    fixed settings to match dataset

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -2,8 +2,11 @@
 
 #Launches 20 different randomizations of the dataset (seq 1 20) with a K of ( -t <number range>)		
 
-for i in `seq 1 20`
-    do less Random_STRUCTURE.qsub | sed s/RUNNUMBER/${i}rand.txt/ > withtemp.qsub
+# Files were randomized by:
+#   for i in `seq 1 20`; do shuf -o R${i}_batch_20170205.structure.tsv batch_20170205.structure.tsv; done
+
+for i in `seq 1 22`
+    do less Random_STRUCTURE.qsub | sed s/RUNNUMBER/R${i}_batch_20170205.structure.tsv/ > withtemp.qsub
     
     qsub withtemp.qsub -N ${i}_STRUCTURE -t 1-22
 
