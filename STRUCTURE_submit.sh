ac739e939d3b9205727b9f40df6619fef45b3b18
Amanda Charbonneau <charbo24@msu.edu>
2017-02-08 11:07:47 -0500

    don't need K=1 or K=2

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -7,13 +7,13 @@ for i in `seq 1 20`; do head -2 batch_20170205.structure.tsv | tail -1 > R${i}_b
 for i in `seq 1 20`; do tail -n +3 batch_20170205.structure.tsv | shuf >> R${i}_batch_20170205.structure.tsv; done
 
 
-for i in `seq 1 22`
+for i in `seq 1 20`
     do less ../bug-free-meme/Random_STRUCTURE.qsub | sed s/RUNNUMBER/R${i}_batch_20170205.structure.tsv/ > withtemp.qsub
     
-    qsub withtemp.qsub -N ${i}_STRUCTURE -t 1-22
+    qsub withtemp.qsub -N ${i}_STRUCTURE -t 3-22
     
     less withtemp.qsub | sed s/36/144/ > withtempLong.qsub
     
-    qsub withtempLong.qsub -N ${i}_STRUCTURE -t 1-22
+    qsub withtempLong.qsub -N ${i}_STRUCTURE -t 3-22
 
 done
