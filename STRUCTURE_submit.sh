b7d674e985eca58fdc1342e751a367b23edad19e
Amanda Charbonneau <charbo24@msu.edu>
2017-02-10 18:54:08 -0500

    fixed path

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -8,6 +8,6 @@ tail -n +2 batch_20170205.structure.tsv > nohead_batch_20170205.structure.tsv
     
     qsub ../bug-free-meme/Random_STRUCTURE.qsub -N STRUCTURE -t 3-10
         
-    qsub withtempLong.qsub -N STRUCTURE_Long -t 12-22
+    qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N STRUCTURE_Long -t 12-22
 
 done
