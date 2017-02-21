e899acda35e833b934feb8fa63e6eb8bbb8a22b9
Amanda Charbonneau <charbo24@msu.edu>
2017-02-20 20:05:59 -0500

    accidently doubled K10

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -19,7 +19,7 @@ head -1 nohead_batch_20170214.structure.tsv > ${rep}_batch_20170214.structure.ts
     
     	qsub ../bug-free-meme/Random_STRUCTURE.qsub -N STRUCTURE -t 3-10 -v thisfile=${rep}_batch_20170214.structure.tsv
         
-    	qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N STRUCTURE -t 10-22 -v thisfile=${rep}_batch_20170214.structure.tsv
+    	qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N STRUCTURE -t 11-22 -v thisfile=${rep}_batch_20170214.structure.tsv
 
 	done
 done
