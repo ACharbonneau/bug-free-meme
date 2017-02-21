38f781de7c3378ee8105b95e597eb1d03ad99dff
Amanda Charbonneau <charbo24@msu.edu>
2017-02-20 20:15:21 -0500

    can't loop good. moved qsub

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -17,11 +17,12 @@ head -1 nohead_batch_20170214.structure.tsv > ${rep}_batch_20170214.structure.ts
 
 		sed "${next}q;d" nohead_batch_20170214.structure.tsv >> ${rep}_batch_20170214.structure.tsv
     
-    	qsub ../bug-free-meme/Random_STRUCTURE.qsub -N STRUCTURE -t 3-10 -v thisfile=${rep}_batch_20170214.structure.tsv
-        
-    	qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N STRUCTURE -t 11-22 -v thisfile=${rep}_batch_20170214.structure.tsv
 
 	done
+    qsub ../bug-free-meme/Random_STRUCTURE.qsub -N STRUCTURE -t 3-10 -v thisfile=${rep}_batch_20170214.structure.tsv
+        
+    qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N STRUCTURE -t 11-22 -v thisfile=${rep}_batch_20170214.structure.tsv
+
 done
     
 
