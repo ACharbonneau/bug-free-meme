19976c3084744fbc6b08893c9553571db766e9f9
Amanda Charbonneau <charbo24@msu.edu>
2017-03-29 20:14:37 -0400

    figure out how many markers and individuals there are

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -4,11 +4,20 @@
 
 BATCH="batch_20170329.structure.tsv"
 
+
 #Randomize Input files
 tail -n +2 ${BATCH} > nohead_${BATCH}
 
+INDIVIDS=$(cat nohead_${BATCH} | wc -l )
+
+MARKERS=$(head -2 ${BATCH} | tail -1 | wc -w)
+
+sed -i s/INDIVIDUALSGOHERE/${INDIVIDS}/ ../bug-free-meme/mainparams4h
+sed -i s/MARKERSGOHERE/${INDIVIDS}/ ../bug-free-meme/mainparams4h
+
+
 for rep in `seq 1 20`
-do seq 2 2 517 | shuf > ${rep}_random.txt
+do seq 2 2 ${INDIVIDS} | shuf > ${rep}_random.txt
 head -1 nohead_${BATCH} > ${rep}_${BATCH}
 
 	for i in `cat ${rep}_random.txt`
