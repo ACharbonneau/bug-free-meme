c91569551390c2d6c3249438316d8022fa49d6e4
Amanda Charbonneau <charbo24@msu.edu>
2017-03-29 20:41:18 -0400

    having bash figure out its own numbers is harder than necessary

--- STRUCTURE_submit.sh
+++ STRUCTURE_submit.sh
@@ -8,16 +8,17 @@ BATCH="batch_20170329.structure.tsv"
 #Randomize Input files
 tail -n +2 ${BATCH} > nohead_${BATCH}
 
-INDIVIDS=$(cat nohead_${BATCH} | wc -l )
+NLINES=$(tail -n +2 nohead_${BATCH} | wc -l )
+INDIVIDS=`expr ${NLINES} / 2`
 
 MARKERS=$(head -2 ${BATCH} | tail -1 | wc -w)
 
 sed s/INDIVIDUALSGOHERE/${INDIVIDS}/ ../bug-free-meme/mainparams4h > mainparams
-sed -i s/MARKERSGOHERE/${INDIVIDS}/ mainparams
+sed -i s/MARKERSGOHERE/${MARKERS}/ mainparams
 
 
 for rep in `seq 1 20`
-do seq 2 2 ${INDIVIDS} | shuf > ${rep}_random.txt
+do seq 2 2 ${NLINES} | shuf > ${rep}_random.txt
 head -1 nohead_${BATCH} > ${rep}_${BATCH}
 
 	for i in `cat ${rep}_random.txt`
@@ -28,8 +29,8 @@ head -1 nohead_${BATCH} > ${rep}_${BATCH}
 
 		sed "${next}q;d" nohead_${BATCH} >> ${rep}_${BATCH}
 
-
 	done
+
     qsub ../bug-free-meme/Random_STRUCTURE.qsub -N ${rep}_STRUCTURE -t 3-5 -v thisfile=${rep}_${BATCH}
 
     qsub ../bug-free-meme/Random_STRUCTURE_Long.qsub -N ${rep}_STRUCTURE -t 6-22 -v thisfile=${rep}_${BATCH}
